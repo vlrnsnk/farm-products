ec62d94b728b8d36ea2b46f30140743e3c026e75
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-05-05 09:40:23 +0000

    Add Header dummy component

--- src/components/layout/page-wrapper/page-wrapper.jsx
+++ src/components/layout/page-wrapper/page-wrapper.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./style.css";
+import Header from "../header/header";
 
 export default function PageWrapper() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
