ca42b780dcd4344694e88b2d1482216b4ef6e1fb
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-05-05 10:41:43 +0000

    Add FeaturesList tag to MainPage

--- src/components/pages/main-page/main-page.jsx
+++ src/components/pages/main-page/main-page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
 import About from "../../blocks/about/about";
+import FeaturesList from "../../blocks/features-list/features-list";
 
 export default function MainPage() {
   return (
     <>
       <About />
+      <FeaturesList />
     </>
   );
 }
