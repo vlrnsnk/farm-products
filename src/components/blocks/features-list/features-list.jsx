b15b0140f31fcba0d68e5bb4f8d39ccd64ac1fbe
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-05-05 14:35:14 +0000

    Add Button dummy component to Header and FeaturesList

--- src/components/blocks/features-list/features-list.jsx
+++ src/components/blocks/features-list/features-list.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./style.css";
+import Button from "../../ui/button/button";
 
 export default function FeaturesList() {
-  return <></>;
+  return (
+    <>
+      <Button />
+    </>
+  );
 }
--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./style.css";
 import Logo from "../../ui/logo/logo";
 import Nav from "../../layout/nav/nav";
+import Button from "../../ui/button/button";
 
 export default function Header() {
   return (
     <header className="header">
       <Logo />
       <Nav />
+      <Button />
     </header>
   );
 }
