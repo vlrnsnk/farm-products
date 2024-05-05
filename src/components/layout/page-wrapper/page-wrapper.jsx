cfb2c6e720682656f76a88ef1907ddc42ef572bd
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-05-05 10:19:42 +0000

    Add MainPage dummy component

--- src/components/layout/page-wrapper/page-wrapper.jsx
+++ src/components/layout/page-wrapper/page-wrapper.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./style.css";
 import Header from "../header/header";
+import MainPage from "../../pages/main-page/main-page";
 
 export default function PageWrapper() {
   return (
     <>
       <Header />
+      <main className="page-wrapper__main">
+        <MainPage />
+      </main>
     </>
   );
 }
