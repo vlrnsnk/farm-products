3a15c1490f2418700f6a2cfc2b7b9f019446462d
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-05-05 15:15:48 +0000

    Add style and logo.svg to Logo

--- src/components/ui/logo/logo.jsx
+++ src/components/ui/logo/logo.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./style.css";
+import logo from "../../../assets/logo.svg";
 
 export default function Logo() {
-  return <></>;
+  return (
+    <a href="/" className="logo__link">
+      <img src={logo} alt="logo" />
+      <span className="logo__text">Farm Products</span>
+    </a>
+  );
 }
