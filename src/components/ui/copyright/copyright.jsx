c1cda3ff966ae9a54972a5b8dec9f9c2ad6dcaa5
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-05-06 06:35:23 +0000

    Add dynamic output of current year in Copyright

--- src/components/ui/copyright/copyright.jsx
+++ src/components/ui/copyright/copyright.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./style.css";
 
+const currentYear = new Date().getFullYear();
+
 export default function Copyright() {
-  return <span>2024</span>;
+  return <span>{currentYear}</span>;
 }
