f8ad6a139485573f78af518c8acc8804a80dd23b
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-05-06 06:51:06 +0000

    Add style to Footer

--- src/components/ui/copyright/copyright.jsx
+++ src/components/ui/copyright/copyright.jsx
@@ -4,5 +4,5 @@ import "./style.css";
 const currentYear = new Date().getFullYear();
 
 export default function Copyright() {
-  return <span>{currentYear}</span>;
+  return <span>Copyright © {currentYear}</span>;
 }
