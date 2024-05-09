dc548f27e583a9d56b9bc14768f98158862b670d
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-05-09 09:29:56 +0000

    Add default theme styling

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./components/app/app";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./themes/default";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
---
+++ src/themes/default.js
@@ -0,0 +1,17 @@
+export const defaultTheme = {
+  colorForBlackText: "#333333",
+  colorWhite: "#ffffff",
+  colorGray: "#f6f6f6",
+  colorForLightBackground: "#fff6d9",
+  colorForDarkBackground: "#ffeead",
+  colorForNewIcon: "#ffb334",
+  colorForSoftIcon: "#7fc92e",
+  colorForButton: "#ff732b",
+  colorForButtonHover: "f35714",
+  pagePadding: "90px",
+  indent: "20px",
+  fontFamily: '"Inter", "Arial", sans-serif',
+  fontSizeDefault: "18px",
+  footerHeight: "80px",
+  pageWidth: "1280px",
+};
