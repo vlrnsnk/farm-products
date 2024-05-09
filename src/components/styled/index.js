e7877bbc835364beb44bbe73139256a1bb15f3b1
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-05-09 18:09:36 +0000

    Add export of styled components

---
+++ src/components/styled/index.js
@@ -0,0 +1,5 @@
+export { default as Img } from "./img/img";
+export { default as Li } from "./li/li";
+export { default as P } from "./p/p";
+export { default as Section } from "./section/section";
+export { default as Ul } from "./ul/ul";
