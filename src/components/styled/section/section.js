7f00de10960712600609a431e322385bdfb7945f
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-05-09 18:09:13 +0000

    Add styled Section component

---
+++ src/components/styled/section/section.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Section = styled.section`
+  position: relative;
+  display: flex;
+  padding-top: 100px;
+  padding-bottom: 100px;
+  padding-left: ${(props) => props.theme.pagePadding};
+  padding-right: ${(props) => props.theme.pagePadding};
+  box-sizing: border-box;
+`;
+
+export default Section;
