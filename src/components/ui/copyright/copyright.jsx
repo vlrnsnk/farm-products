5999b3b7b924209979f3f470637f70972eb1188e
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-05-16 11:41:13 +0000

    Remove UI Copyright component

--- src/components/ui/copyright/copyright.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-const currentYear = new Date().getFullYear();
-
-export default function Copyright() {
-  return <span>Copyright © {currentYear}</span>;
-}
