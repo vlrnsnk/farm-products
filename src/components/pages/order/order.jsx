d75c53bd3c1a62a3c03cfa7e84ca7d21e15a09c4
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-06-09 08:46:26 +0000

    Add Panel to Order

--- src/components/pages/order/order.jsx
+++ src/components/pages/order/order.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { StyledOrder, LeftColumn } from "./styles";
+import Panel from "../../../components/ui/panel/panel";
 
 function Order() {
   return (
     <StyledOrder as="form">
-      <LeftColumn></LeftColumn>
+      <LeftColumn>
+        <Panel marginBottom={20} paddingTop={24} paddingBottom={10}></Panel>
+        <Panel></Panel>
+      </LeftColumn>
       <div>slider with products goes here</div>
     </StyledOrder>
   );
---
+++ src/components/ui/panel/panel.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { Section } from "../../styled";
+
+const Panel = styled(Section)`
+  padding: ${(props) => props.theme.indent};
+  padding-top: ${(props) => `${props.paddingTop}px` || props.themeIndent};
+  padding-bottom: ${(props) =>
+    `${props.paddingBottom}px` || props.theme.indent};
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  box-shadow:
+    0 10px 20px rgba(0, 0, 0, 0.04),
+    0 2px 6px rgba(0, 0, 0, 0.04),
+    0 0 1px rgba(0, 0, 0, 0.04);
+  margin-bottom: ${(props) => props.marginBottom || 0}px;
+  width: ${(props) => props.$width || "auto"};
+  display: block;
+  background-color: ${(props) => props.theme.colorWhite};
+`;
+
+export default Panel;
