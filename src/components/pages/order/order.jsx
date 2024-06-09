887aff3bc09361d8223d784f07e3d06b1dff94c8
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-06-09 08:50:44 +0000

    Add Title to Order

--- src/components/pages/order/order.jsx
+++ src/components/pages/order/order.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { StyledOrder, LeftColumn } from "./styles";
 import Panel from "../../../components/ui/panel/panel";
+import Title, { TitleSize } from "../../ui/title/title";
 
 function Order() {
   return (
     <StyledOrder as="form">
       <LeftColumn>
-        <Panel marginBottom={20} paddingTop={24} paddingBottom={10}></Panel>
-        <Panel></Panel>
+        <Panel marginBottom={20} paddingTop={24} paddingBottom={10}>
+          <Title as="h2" size={TitleSize.EXTRA_SMALL} marginBottom={12}>
+            Choose your products
+          </Title>
+          Checkbox with products list
+        </Panel>
+        <Panel>
+          <Title size={TitleSize.EXTRA_SMALL} marginBottom={24}>
+            Place order
+          </Title>
+        </Panel>
       </LeftColumn>
       <div>slider with products goes here</div>
     </StyledOrder>
