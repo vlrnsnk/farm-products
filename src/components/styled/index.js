3dc761a19728d1292a76d02a5c9c453bb18bbf39
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-06-09 09:08:49 +0000

    Add Label to styled components

--- src/components/styled/index.js
+++ src/components/styled/index.js
@@ -3,3 +3,4 @@ export { default as Li } from "./li/li";
 export { default as P } from "./p/p";
 export { default as Section } from "./section/section";
 export { default as Ul } from "./ul/ul";
+export { default as Label } from "./label/label";
