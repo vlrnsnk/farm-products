e28cee0bf24167c502105bfef13f8bb37b9076b4
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-06-10 09:36:12 +0000

    Export default VisuallyHiddenInput from styled index.js

--- src/components/styled/index.js
+++ src/components/styled/index.js
@@ -5,3 +5,4 @@ export { default as Section } from "./section/section";
 export { default as Ul } from "./ul/ul";
 export { default as Label } from "./label/label";
 export { default as Button } from "./button/button";
+export { default as VisuallyHiddenInput } from "./visually-hidden-input/visually-hidden-input";
