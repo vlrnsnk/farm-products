3512dad81b7c2edb688fa619c264b11be850b201
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-06-10 11:21:09 +0000

    Add Price ui component

---
+++ src/components/ui/price/price.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyledPrice } from "./styles";
+
+const formatPrice = (value) => {
+  const roundedPrice = Math.round(value);
+  return roundedPrice.toString().replace(/(\d)(?=(\d\d\d)+$)/, "$1 ");
+};
+
+function Price({ value, className }) {
+  return (
+    <StyledPrice className={className}>{formatPrice(value)} ₴</StyledPrice>
+  );
+}
+
+export default Price;
---
+++ src/components/ui/price/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledPrice = styled.span`
+  display: block;
+  font-size: 24px;
+  font-weight: bold;
+`;
