6b3eeef3b0779f812cdec76a8209f821a3c453eb
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-06-10 09:33:19 +0000

    Add VisuallyHiddenInput ui component

---
+++ src/components/ui/visually-hidden-input/visually-hidden-input.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const VisuallyHiddenInput = styled.input`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  padding: 0;
+  border: 0;
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+
+  &:focus-visible ~ * {
+    outline: 1px solid #000000;
+  }
+`;
+
+export default VisuallyHiddenInput;
