71b1e6365a8e4ed0116b1bb6fe02aa60a818ad31
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-06-10 06:13:20 +0000

    Add dummy Tabs ui component

---
+++ src/components/ui/tabs/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { Ul } from "../../styled";
+
+export const Header = styled(Ul)``;
+
+export const Content = styled.div``;
---
+++ src/components/ui/tabs/tabs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header, Content } from "./styles";
+
+function Tabs() {
+  return (
+    <div>
+      <Header></Header>
+      <Content></Content>
+    </div>
+  );
+}
+
+export default Tabs;
