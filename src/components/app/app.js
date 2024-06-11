6191f152ccd298333940f27a2aca2a61f5b00b12
vlrnsnk <168164564+vlrnsnk@users.noreply.github.com>
2024-06-11 16:20:17 +0000

    Add dummy routing to App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,14 +3,30 @@ import { GlobalStyle } from "./styles";
 import PageWrapper from "../layout/page-wrapper/page-wrapper";
 import features from "../../mocks/features";
 import products from "../../mocks/products";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppRoute } from "../../const.js";
 
-export default function App() {
+function App() {
   return (
     <>
       <GlobalStyle />
-      <PageWrapper features={features} products={products}>
-        Page content
-      </PageWrapper>
+      <Router>
+        <Routes>
+          <Route exact path={AppRoute.MAIN}>
+            <Route
+              index
+              element={<PageWrapper features={features} products={products} />}
+            />
+            <Route
+              exact
+              path={AppRoute.ORDER}
+              element={<PageWrapper features={features} products={products} />}
+            />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
+
+export default App;
